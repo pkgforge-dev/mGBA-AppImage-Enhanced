d2a9832a9594171f15aabe13b83ab81a57add78a
Link4Electronics <link4electronics@gmail.com>
2026-04-01 11:43:02 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm
+pacman -Syu --noconfirm lua
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
