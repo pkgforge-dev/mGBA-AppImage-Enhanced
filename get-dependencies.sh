921a5f6ebd209b521beb8ab43c0ea312089805d7
Link4Electronics <link4electronics@gmail.com>
2026-04-19 23:48:49 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,24 +6,41 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm lua
+pacman -Syu --noconfirm \
+    cmake              \
+    desktop-file-utils \
+    kvantum            \
+    libepoxy           \
+    libmgba            \
+    libzip             \
+    lua                \
+    lxqt-qtplugin      \
+    qt6-multimedia     \
+    qt6-tools          \
+    qt6ct              \
+    sdl2-compat        \
+    vulkan-headers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
- 	package=mgba-git
-  make-aur-package $package
+  echo "Building nightly version of mGBA..."
+  echo "---------------------------------------------------------------"
+  REPO="https://github.com/mgba-emu/mgba"
+  VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+  git clone "$REPO" ./mgba
+  echo "$VERSION" > ~/version
+
+  cmake -S ./mgba -B build \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DUSE_MINIZIP=OFF
+  cmake --build build --config Release
+  cmake --install build
 else
-  package=mgba-qt
-  pacman -Syu --noconfirm $package
+  pacman -S --noconfirm mgba-qt
+  pacman -Q mgba-qt | awk '{print $2; exit}' > ~/version
 fi
-pacman -Q "$package" | awk '{print $2; exit}' > ~/version
