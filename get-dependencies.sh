cd93c417e94cb0d0a2601cbb7d0c6f0007bf74bc
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-19 22:13:13 -0400

    add `ffmpeg-mini` (#7)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ pacman -Syu --noconfirm lua
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
